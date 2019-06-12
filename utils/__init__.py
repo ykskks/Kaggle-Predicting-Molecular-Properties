b078455f513290280f8520b1ff78afeb53aace0e
= <fkt.kohei@gmail.com>
2019-06-13 01:01:30 +0900

    add function for calculating atom env feature

--- utils/__init__.py
+++ utils/__init__.py
@@ -56,3 +56,113 @@ def log_evaluation(logger, period=1, show_stdv=True, level=logging.DEBUG):
             logger.log(level, '[{}]\t{}'.format(env.iteration+1, result))
     _callback.order = 10
     return _callback
+
+def get_atom_env_feature(df):
+    a0_env, a1_env = {}, {}
+
+    a0_env['degree'], a1_env['degree'] = [], []
+    a0_env['explicit_valence'], a1_env['explicit_valence'] = [], []
+    a0_env['hybridization'], a1_env['hybridization'] = [], []
+    a0_env['implicit_valence'], a1_env['implicit_valence'] = [], []
+    a0_env['is_aromatic'], a1_env['is_aromatic'] = [], []
+    a0_env['mass'], a1_env['mass'] = [], []
+    a0_env['total_degree'], a1_env['total_degree'] = [], []
+    a0_env['total_hs'], a1_env['total_hs'] = [], []
+    a0_env['total_valence'], a1_env['total_valence'] = [], []
+    a0_env['in_ring'], a1_env['in_ring'] = [], []
+    a0_env['in_ring3'], a1_env['in_ring3'] = [], []
+    a0_env['in_ring4'], a1_env['in_ring4'] = [], []
+    a0_env['in_ring5'], a1_env['in_ring5'] = [], []
+    a0_env['in_ring6'], a1_env['in_ring6'] = [], []
+    a0_env['in_ring7'], a1_env['in_ring7'] = [], []
+    a0_env['in_ring8'], a1_env['in_ring8'] = [], []
+
+    cols = ['degree', 'explicit_valence', 'hybridization', 'implicit_valence', 'is_aromatic', 'mass',
+            'total_degree', 'total_hs', 'total_valence', 'in_ring', 'in_ring3', 'in_ring4', 'in_ring5',
+            'in_ring6', 'in_ring7', 'in_ring8']
+
+    for idx, row in df.iterrows():
+        try:
+            a0 = row['mols'].GetAtomWithIdx(row['atom_index_0'])
+            a1 = row['mols'].GetAtomWithIdx(row['atom_index_1'])
+
+            a0_env['degree'].append(a0.GetDegree())
+            a0_env['explicit_valence'].append(a0.GetExplicitValence())
+            a0_env['hybridization'].append(a0.GetHybridization())
+            a0_env['implicit_valence'].append(a0.GetImplicitValence())
+            a0_env['is_aromatic'].append(a0.GetIsAromatic())
+            a0_env['mass'].append(a0.GetMass())
+            a0_env['total_degree'].append(a0.GetTotalDegree())
+            a0_env['total_hs'].append(a0.GetTotalNumHs())
+            a0_env['total_valence'].append(a0.GetTotalValence())
+            a0_env['in_ring'].append(a0.IsInRing())
+            a0_env['in_ring3'].append(a0.IsInRingSize(3))
+            a0_env['in_ring4'].append(a0.IsInRingSize(4))
+            a0_env['in_ring5'].append(a0.IsInRingSize(5))
+            a0_env['in_ring6'].append(a0.IsInRingSize(6))
+            a0_env['in_ring7'].append(a0.IsInRingSize(7))
+            a0_env['in_ring8'].append(a0.IsInRingSize(8))
+
+            a1_env['degree'].append(a1.GetDegree())
+            a1_env['explicit_valence'].append(a1.GetExplicitValence())
+            a1_env['hybridization'].append(a1.GetHybridization())
+            a1_env['implicit_valence'].append(a1.GetImplicitValence())
+            a1_env['is_aromatic'].append(a1.GetIsAromatic())
+            a1_env['mass'].append(a1.GetMass())
+            a1_env['total_degree'].append(a1.GetTotalDegree())
+            a1_env['total_hs'].append(a1.GetTotalNumHs())
+            a1_env['total_valence'].append(a1.GetTotalValence())
+            a1_env['in_ring'].append(a1.IsInRing())
+            a1_env['in_ring3'].append(a1.IsInRingSize(3))
+            a1_env['in_ring4'].append(a1.IsInRingSize(4))
+            a1_env['in_ring5'].append(a1.IsInRingSize(5))
+            a1_env['in_ring6'].append(a1.IsInRingSize(6))
+            a1_env['in_ring7'].append(a1.IsInRingSize(7))
+            a1_env['in_ring8'].append(a1.IsInRingSize(8))
+
+        except:
+            a0_env['degree'].append(np.nan)
+            a0_env['explicit_valence'].append(np.nan)
+            a0_env['hybridization'].append(np.nan)
+            a0_env['implicit_valence'].append(np.nan)
+            a0_env['is_aromatic'].append(np.nan)
+            a0_env['mass'].append(np.nan)
+            a0_env['total_degree'].append(np.nan)
+            a0_env['total_hs'].append(np.nan)
+            a0_env['total_valence'].append(np.nan)
+            a0_env['in_ring'].append(np.nan)
+            a0_env['in_ring3'].append(np.nan)
+            a0_env['in_ring4'].append(np.nan)
+            a0_env['in_ring5'].append(np.nan)
+            a0_env['in_ring6'].append(np.nan)
+            a0_env['in_ring7'].append(np.nan)
+            a0_env['in_ring8'].append(np.nan)
+
+            a1_env['degree'].append(np.nan)
+            a1_env['explicit_valence'].append(np.nan)
+            a1_env['hybridization'].append(np.nan)
+            a1_env['implicit_valence'].append(np.nan)
+            a1_env['is_aromatic'].append(np.nan)
+            a1_env['mass'].append(np.nan)
+            a1_env['total_degree'].append(np.nan)
+            a1_env['total_hs'].append(np.nan)
+            a1_env['total_valence'].append(np.nan)
+            a1_env['in_ring'].append(np.nan)
+            a1_env['in_ring3'].append(np.nan)
+            a1_env['in_ring4'].append(np.nan)
+            a1_env['in_ring5'].append(np.nan)
+            a1_env['in_ring6'].append(np.nan)
+            a1_env['in_ring7'].append(np.nan)
+            a1_env['in_ring8'].append(np.nan)
+      
+
+    for a in ['a0', 'a1']:
+        for col in cols:
+            if a == 'a0':
+                df[a + '_' + col] = a0_env[col]
+            else:
+                df[a + '_' + col] = a1_env[col]
+
+    cols = [col for col in df.columns if ('a0' in col) or ('a1' in col)]
+
+    return df, cols
