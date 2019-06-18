6f24107b979f159b24cfde83986805ecc7ab0af7
ykskks <fkt.kohei@gmail.com>
2019-06-18 14:32:23 +0900

    add coumlobm interaction feature

--- features/create.py
+++ features/create.py
@@ -9,6 +9,7 @@ import numpy as np
 import feather
 from rdkit.Chem import Descriptors, Descriptors3D, MolFromMolBlock, MACCSkeys, DataStructs
 from tqdm import tqdm
+from joblib import Parallel, delayed
 
 from features.base import get_arguments, get_features, generate_features, Feature
 from utils import get_atom_env_feature, get_atom_neighbor_feature, calc_atom_neighbor_feature, reduce_mem_usage
@@ -41,18 +42,10 @@ class AtomPosition(Feature):
         test = map_atom_info(test, 0)
         test = map_atom_info(test, 1)   
 
-        self.train['x_0'] = train['x_0']
-        self.train['x_1'] = train['x_1']
-        self.train['y_0'] = train['y_0']
-        self.train['y_1'] = train['y_1']
-        self.train['z_0'] = train['z_0']
-        self.train['z_1'] = train['z_1']        
-        self.test['x_0'] = test['x_0']
-        self.test['x_1'] = test['x_1']
-        self.test['y_0'] = test['y_0']
-        self.test['y_1'] = test['y_1']
-        self.test['z_0'] = test['z_0']
-        self.test['z_1'] = test['z_1']     
+        pos_list = [i+'_'+j for i in ['x', 'y', 'z'] for j in ['0', '1']]
+        for pos in pos_list:
+            self.train[pos] = train[pos]
+            self.test[pos] = test[pos]
 
 
 class Atom(Feature):
@@ -209,14 +202,14 @@ class AtomNeighbors(Feature):
             self.test[col] = test[col]      
 
 
-#https://www.kaggle.com/brandenkmurray/coulomb-interaction-parallelized
+# https://www.kaggle.com/hervind/speed-up-coulomb-interaction-56x-faster
 class CoulombInteraction(Feature):
     def create_features(self):
         global train, test
         train.drop('scalar_coupling_constant', axis=1, inplace=True)
         total = pd.concat([train, test], axis=0)
         structures = feather.read_dataframe('./data/input/structures.feather')
-        NCORES = 2
+        NCORES = 6
         NUM = 5
 
         train = reduce_mem_usage(train)
@@ -225,88 +218,74 @@ class CoulombInteraction(Feature):
         structures = reduce_mem_usage(structures)
         gc.collect()
 
-        def get_dist_matrix(structures, molecule):
-            df_temp = structures.query('molecule_name == "{}"'.format(molecule))
-            locs = df_temp[['x','y','z']].values
-            num_atoms = len(locs)
-            loc_tile = np.tile(locs.T, (num_atoms,1,1))
-            dist_mat = ((loc_tile - loc_tile.T)**2).sum(axis=1)
-            return dist_mat
+        df_distance = structures.merge(structures, how = 'left', on= 'molecule_name', suffixes = ('_0', '_1'))
+        # remove same molecule
+        df_distance = df_distance.loc[df_distance['atom_index_0'] != df_distance['atom_index_1']]
 
-        def assign_atoms_index(df, molecule):
-            se_0 = df.query('molecule_name == "{}"'.format(molecule))['atom_index_0']
-            se_1 = df.query('molecule_name == "{}"'.format(molecule))['atom_index_1']
-            assign_idx = pd.concat([se_0, se_1]).unique()
-            assign_idx.sort()
-            return assign_idx
+        df_distance['distance'] = np.linalg.norm(df_distance[['x_0','y_0', 'z_0']].values - 
+                                                df_distance[['x_1', 'y_1', 'z_1']].values, axis=1, ord = 2)
 
-        def get_pickup_dist_matrix(df, structures, molecule, num_pickup=NUM, atoms=['H', 'C', 'N', 'O', 'F']):
-            pickup_dist_matrix = np.zeros([0, len(atoms)*num_pickup])
-            assigned_idxs = assign_atoms_index(df, molecule) # [0, 1, 2, 3, 4, 5, 6] -> [1, 2, 3, 4, 5, 6]
-            dist_mat = get_dist_matrix(structures, molecule)
-            for idx in assigned_idxs: # [1, 2, 3, 4, 5, 6] -> [2]
-                dist_arr = dist_mat[idx] # (7, 7) -> (7, )
-
-                atoms_mole = structures.query('molecule_name == "{}"'.format(molecule))['atom'].values # ['O', 'C', 'C', 'N', 'H', 'H', 'H']
-                atoms_mole_idx = structures.query('molecule_name == "{}"'.format(molecule))['atom_index'].values # [0, 1, 2, 3, 4, 5, 6]
-
-                mask_atoms_mole_idx = atoms_mole_idx != idx # [ True,  True, False,  True,  True,  True,  True]
-                masked_atoms = atoms_mole[mask_atoms_mole_idx] # ['O', 'C', 'N', 'H', 'H', 'H']
-                masked_atoms_idx = atoms_mole_idx[mask_atoms_mole_idx]  # [0, 1, 3, 4, 5, 6]
-                masked_dist_arr = dist_arr[mask_atoms_mole_idx]  # [ 5.48387003, 2.15181049, 1.33269675, 10.0578779, 4.34733927, 4.34727838]
-
-                sorting_idx = np.argsort(masked_dist_arr) # [2, 1, 5, 4, 0, 3]
-                sorted_atoms_idx = masked_atoms_idx[sorting_idx] # [3, 1, 6, 5, 0, 4]
-                sorted_atoms = masked_atoms[sorting_idx] # ['N', 'C', 'H', 'H', 'O', 'H']
-                sorted_dist_arr = 1 / masked_dist_arr[sorting_idx] #[0.75035825,0.46472494,0.23002898,0.23002576,0.18235297,0.09942455]
-
-                target_matrix = np.zeros([len(atoms), num_pickup])
-                for a, atom in enumerate(atoms):
-                    pickup_atom = sorted_atoms == atom # [False, False,  True,  True, False,  True]
-                    pickup_dist = sorted_dist_arr[pickup_atom] # [0.23002898, 0.23002576, 0.09942455]
-                    num_atom = len(pickup_dist)
-                    if num_atom > num_pickup:
-                        target_matrix[a, :] = pickup_dist[:num_pickup]
-                    else:
-                        target_matrix[a, :num_atom] = pickup_dist
-                pickup_dist_matrix = np.vstack([pickup_dist_matrix, target_matrix.reshape(-1)])
-            return pickup_dist_matrix
-
-        mols = total['molecule_name'].unique()
-        dist_mat = np.zeros([0, NUM*5])
-        atoms_idx = np.zeros([0], dtype=np.int32)
-        molecule_names = np.empty([0])
-
-        for mol in tqdm(mols):
-            assigned_idxs = assign_atoms_index(total, mol)
-            dist_mat_mole = get_pickup_dist_matrix(total, structures, mol, num_pickup=NUM)
-            mol_name_arr = [mol] * len(assigned_idxs) 
+        def get_interaction_data_frame(df_distance, num_nearest = 5):
+            print("START")
             
-            molecule_names = np.hstack([molecule_names, mol_name_arr])
-            atoms_idx = np.hstack([atoms_idx, assigned_idxs])
-            dist_mat = np.vstack([dist_mat, dist_mat_mole])
+            # get nearest 5 (num_nearest) by distances
+            df_temp = df_distance.groupby(['molecule_name', 'atom_index_0', 'atom_1'])['distance'].nsmallest(num_nearest)
+            
+            # make it clean
+            df_temp = pd.DataFrame(df_temp).reset_index()[['molecule_name', 'atom_index_0', 'atom_1', 'distance']]
+            df_temp.columns = ['molecule_name', 'atom_index', 'atom', 'distance']
+            
+            print("Time Nearest")
+            
+            # get rank by distance
+            df_temp['distance_rank'] = df_temp.groupby(['molecule_name', 'atom_index', 'atom'])['distance'].rank(ascending = True, method = 'first').astype(int)
+            
+            print("Time Rank")
+            
+            # pivot to get nearest distance by atom type 
+            df_distance_nearest = pd.pivot_table(df_temp, index = ['molecule_name','atom_index'], columns= ['atom', 'distance_rank'], values= 'distance')
+            
+            print("Time Pivot")
+            del df_temp
+            
+            columns_distance_nearest =  np.core.defchararray.add('distance_nearest_', 
+                                                np.array(df_distance_nearest.columns.get_level_values('distance_rank')).astype(str) +  
+                                                np.array(df_distance_nearest.columns.get_level_values('atom')) )
+            df_distance_nearest.columns = columns_distance_nearest
+            
+            # 1 / r^2 to get the square inverse same with the previous kernel
+            df_distance_sq_inv_farthest = 1 / (df_distance_nearest ** 2)
+            
+            columns_distance_sq_inv_farthest = [col.replace('distance_nearest', 'dist_sq_inv_') for col in columns_distance_nearest]
 
-        col_name_list = []
-        atoms = ['H', 'C', 'N', 'O', 'F']
-        for a in atoms:
-            for n in range(NUM):
-                col_name_list.append('dist_{}_{}'.format(a, n))
-                
-        se_mole = pd.Series(molecule_names, name='molecule_name')
-        se_atom_idx = pd.Series(atoms_idx, name='atom_index')
-        df_dist = pd.DataFrame(dist_mat, columns=col_name_list)
-        df_distance = pd.concat([se_mole, se_atom_idx,df_dist], axis=1)
+            df_distance_sq_inv_farthest.columns = columns_distance_sq_inv_farthest
+    
+            print("Time Inverse Calculation")
+            
+            df_interaction = pd.concat([df_distance_sq_inv_farthest, df_distance_nearest] , axis = 1)
+            df_interaction.reset_index(inplace = True)
+      
+            print("Time Concat")
+            
+            return df_interaction
+
+
+        df_interaction = get_interaction_data_frame(df_distance)
 
         #merge with train, test
-        train = train.merge(df_distance, left_on=['molecule_name', 'atom_index_0'], right_on=['molecule_name', 'atom_index'])
-        train = train.merge(df_distance, left_on=['molecule_name', 'atom_index_1'], right_on=['molecule_name', 'atom_index'])
+        train = train.merge(df_interaction, left_on=['molecule_name', 'atom_index_0'], right_on=['molecule_name', 'atom_index'])
+        train = train.merge(df_interaction, left_on=['molecule_name', 'atom_index_1'], right_on=['molecule_name', 'atom_index'])
 
-        test = test.merge(df_distance, left_on=['molecule_name', 'atom_index_0'], right_on=['molecule_name', 'atom_index'])
-        test = test.merge(df_distance, left_on=['molecule_name', 'atom_index_1'], right_on=['molecule_name', 'atom_index'])
+        test = test.merge(df_interaction, left_on=['molecule_name', 'atom_index_0'], right_on=['molecule_name', 'atom_index'])
+        test = test.merge(df_interaction, left_on=['molecule_name', 'atom_index_1'], right_on=['molecule_name', 'atom_index'])
 
+        col_name_list = [col for col in train.columns if 'sq_inv' in col]
+        print(f'Number of cols generated is {len(col_name_list)}')
+       
         for col in col_name_list:
             self.train[col] = train[col]
-            self.test[col] = test[col]           
+            self.test[col] = test[col]     
+     
 
 
 
