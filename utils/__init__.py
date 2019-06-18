a253938e6d79fef2580d579878b999d3ce543938
ykskks <fkt.kohei@gmail.com>
2019-06-18 14:34:22 +0900

    update reduce_mem_usage

--- utils/__init__.py
+++ utils/__init__.py
@@ -65,11 +65,13 @@ def reduce_mem_usage(df):
     """
     start_mem = df.memory_usage().sum() / 1024**2
     print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
     
     for col in df.columns:
         col_type = df[col].dtype
         
-        if col_type != object:
+        if col_type in numerics:
             c_min = df[col].min()
             c_max = df[col].max()
             if str(col_type)[:3] == 'int':
@@ -82,9 +84,7 @@ def reduce_mem_usage(df):
                 elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
                     df[col] = df[col].astype(np.int64)  
             else:
-                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
-                    df[col] = df[col].astype(np.float16)
-                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                if c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
                     df[col] = df[col].astype(np.float32)
                 else:
                     df[col] = df[col].astype(np.float64)
