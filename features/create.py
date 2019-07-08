fd5052f97c6d1471d859dbd8c0f034106c3353e1
= <fkt.kohei@gmail.com>
2019-07-08 13:19:37 +0900

    create more distance based features

--- features/create.py
+++ features/create.py
@@ -79,6 +79,9 @@ class AtomDistance(Feature):
         structures = feather.read_dataframe('./data/input/structures.feather')
         global train, test
 
+        radius = dict(zip(['H','C','N','O','F'], [0.38,0.77,0.75,0.73,0.71]))
+        #en = dict(zip(['H','C','N','O','F'], [2.2,2.55,3.04,3.44,3.98]))
+
         def map_atom_info(df, atom_idx):
             df = pd.merge(df, structures, how='left', left_on=['molecule_name', f'atom_index_{atom_idx}'],
                           right_on=['molecule_name', 'atom_index'])
@@ -98,8 +101,54 @@ class AtomDistance(Feature):
         test_p_0 = test[['x_0', 'y_0', 'z_0']].values
         test_p_1 = test[['x_1', 'y_1', 'z_1']].values
 
-        self.train['atom_distance'] = np.linalg.norm(train_p_0 - train_p_1, axis=1)
-        self.test['atom_distance'] = np.linalg.norm(test_p_0 - test_p_1, axis=1)
+        train['atom_distance'] = np.linalg.norm(train_p_0 - train_p_1, axis=1)
+        test['atom_distance'] = np.linalg.norm(test_p_0 - test_p_1, axis=1)
+
+        tmp_0 = pd.DataFrame(train.groupby(['molecule_name', 'atom_index_0'])['atom_distance'].apply(lambda x: (1 / x ** 3))).rename(columns={'atom_distance': 'dist_inv_0'})
+        tmp_1 = pd.DataFrame(train.groupby(['molecule_name', 'atom_index_1'])['atom_distance'].apply(lambda x: (1 / x ** 3))).rename(columns={'atom_distance': 'dist_inv_1'})
+
+        tmp_0['molecule_name'], tmp_0['atom_index_0'] = train['molecule_name'], train['atom_index_0']
+        tmp_1['molecule_name'], tmp_1['atom_index_1'] = train['molecule_name'], train['atom_index_1']
+
+        tmp_0 = tmp_0.groupby(['molecule_name', 'atom_index_0'])['dist_inv_0'].sum().reset_index().rename(columns={'dist_inv_0':'dist_inv_0_sum'})
+        tmp_1 = tmp_1.groupby(['molecule_name', 'atom_index_1'])['dist_inv_1'].sum().reset_index().rename(columns={'dist_inv_1':'dist_inv_1_sum'})
+
+        for df in [train, test]:
+            df = df.merge(tmp_0, on=['molecule_name', 'atom_index_0'], how='left')
+            df = df.merge(tmp_1, on=['molecule_name', 'atom_index_1'], how='left')
+            df['dist_inv_sum_pair'] = df['dist_inv_0_sum']*df['dist_inv_1_sum'] / (df['dist_inv_0_sum']+df['dist_inv_1_sum'])
+
+        # some more dist related feature using radius 
+        for df in [train, test]:
+            df['radius_0'] = df['atom_x'].map(radius)
+            df['en_0'] = df['atom_x'].map(en)
+            df['radius_1'] = df['atom_y'].map(radius)
+            df['en_1'] = df['atom_y'].map(en)
+
+            df['bond_length'] = df['atom_distance'] - df['radius_0'] - df['radius_1']
+
+            tmp_0 = df.groupby(['molecule_name', 'atom_index_0'])['bond_length'].agg(['mean', 'max', 'min', 'sum']).reset_index()
+            tmp_0.columns = ['molecule_name', 'atom_index_0', 'bond_length_mean', 'bond_length_max', 'bond_length_min', 'bond_length_sum']
+            tmp_1 = df.groupby(['molecule_name', 'atom_index_1'])['bond_length'].agg(['mean', 'max', 'min']).reset_index()
+            tmp_1.columns = ['molecule_name', 'atom_index_1', 'bond_length_mean', 'bond_length_max', 'bond_length_min', 'bond_length_sum']
+
+            df = df.merge(tmp_0, on=['molecule_name', 'atom_index_0'], how='left')
+            df = df.merge(tmp_1, on=['molecule_name', 'atom_index_1'], how='left', suffixes=['_a0', '_a1'])
+
+            df['bond_length_mean_pair'] = df['bond_length_mean_a0'] + df['bond_length_mean_a1']
+            df['bond_length_max_pair'] = df['bond_length_max_a0'] + df['bond_length_max_a1']
+            df['bond_length_min_pair'] = df['bond_length_min_a0'] + df['bond_length_min_a1']
+            df['bond_length_sum_pair'] = df['bond_length_sum_a0'] + df['bond_length_sum_a1']
+
+
+        cols = ['atom_distance', 'dist_inv_0_sum', 'dist_inv_1_sum', 'dist_inv_sum_pair', 
+                'bond_length_mean_a0', 'bond_length_max_a0', 'bond_length_min_a0', 'bond_length_sum_a0',
+                'bond_length_mean_a1', 'bond_length_max_a1', 'bond_length_min_a1', 'bond_length_sum_a1',
+                'bond_length_mean_pair', 'bond_length_max_pair', 'bond_length_min_pair', 'bond_length_sum_pair']
+
+        for col in cols:
+            self.train[col] = train[col]
+            self.test[col] = test[col]
 
 
 class RdkitDescriptors(Feature):
