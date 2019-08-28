29aac518b956d69bbceb618ad59ff88476beb5a9
ykskks <fkt.kohei@gmail.com>
2019-08-29 05:22:10 +0900

    add DIAYN feature

--- features/create.py
+++ features/create.py
@@ -1029,7 +1029,200 @@ class CisTrans(Feature):
 
         for col in new_cols:
             self.train[col] = train[col]
-            self.test[col] = test[col]      
+            self.test[col] = test[col]     
+
+
+class DisIsAllYouNeed(Feature):
+    def create_features(self):
+
+        DATA_PATH = './data/input'
+        ATOMIC_NUMBERS = {
+            'H': 1,
+            'C': 6,
+            'N': 7,
+            'O': 8,
+            'F': 9
+        }     
+
+        train_dtypes = {
+            'molecule_name': 'category',
+            'atom_index_0': 'int8',
+            'atom_index_1': 'int8',
+            'type': 'category',
+            'scalar_coupling_constant': 'float32'
+        }
+
+        train_csv = pd.read_csv(f'{DATA_PATH}/train.csv', index_col='id', dtype=train_dtypes)
+        train_csv['molecule_index'] = train_csv.molecule_name.str.replace('dsgdb9nsd_', '').astype('int32')
+        train_csv = train_csv[['molecule_index', 'atom_index_0', 'atom_index_1', 'type', 'scalar_coupling_constant']]
+
+        test_csv = pd.read_csv(f'{DATA_PATH}/test.csv', index_col='id', dtype=train_dtypes)
+        test_csv['molecule_index'] = test_csv['molecule_name'].str.replace('dsgdb9nsd_', '').astype('int32')
+        test_csv = test_csv[['molecule_index', 'atom_index_0', 'atom_index_1', 'type']]
+
+        structures_dtypes = {
+            'molecule_name': 'category',
+            'atom_index': 'int8',
+            'atom': 'category',
+            'x': 'float32',
+            'y': 'float32',
+            'z': 'float32'
+        }
+        structures_csv = pd.read_csv(f'{DATA_PATH}/structures.csv', dtype=structures_dtypes)
+        structures_csv['molecule_index'] = structures_csv.molecule_name.str.replace('dsgdb9nsd_', '').astype('int32')
+        structures_csv = structures_csv[['molecule_index', 'atom_index', 'atom', 'x', 'y', 'z']]
+        structures_csv['atom'] = structures_csv['atom'].replace(ATOMIC_NUMBERS).astype('int8')
+
+        def build_type_dataframes(base, structures, coupling_type):
+            base = base[base['type'] == coupling_type].drop('type', axis=1).copy()
+            base = base.reset_index()
+            base['id'] = base['id'].astype('int32')
+            structures = structures[structures['molecule_index'].isin(base['molecule_index'])]
+            return base, structures
+
+        def add_coordinates(base, structures, index):
+            df = pd.merge(base, structures, how='inner',
+                        left_on=['molecule_index', f'atom_index_{index}'],
+                        right_on=['molecule_index', 'atom_index']).drop(['atom_index'], axis=1)
+            df = df.rename(columns={
+                'atom': f'atom_{index}',
+                'x': f'x_{index}',
+                'y': f'y_{index}',
+                'z': f'z_{index}'
+            })
+            return df
+
+        def add_atoms(base, atoms):
+            df = pd.merge(base, atoms, how='inner',
+                        on=['molecule_index', 'atom_index_0', 'atom_index_1'])
+            return df
+
+        def merge_all_atoms(base, structures):
+            df = pd.merge(base, structures, how='left',
+                        left_on=['molecule_index'],
+                        right_on=['molecule_index'])
+            df = df[(df.atom_index_0 != df.atom_index) & (df.atom_index_1 != df.atom_index)]
+            return df
+
+        def add_center(df):
+            df['x_c'] = ((df['x_1'] + df['x_0']) * np.float32(0.5))
+            df['y_c'] = ((df['y_1'] + df['y_0']) * np.float32(0.5))
+            df['z_c'] = ((df['z_1'] + df['z_0']) * np.float32(0.5))
+
+        def add_distance_to_center(df):
+            df['d_c'] = ((
+                (df['x_c'] - df['x'])**np.float32(2) +
+                (df['y_c'] - df['y'])**np.float32(2) + 
+                (df['z_c'] - df['z'])**np.float32(2)
+            )**np.float32(0.5))
+
+        def add_distance_between(df, suffix1, suffix2):
+            df[f'd_{suffix1}_{suffix2}'] = ((
+                (df[f'x_{suffix1}'] - df[f'x_{suffix2}'])**np.float32(2) +
+                (df[f'y_{suffix1}'] - df[f'y_{suffix2}'])**np.float32(2) + 
+                (df[f'z_{suffix1}'] - df[f'z_{suffix2}'])**np.float32(2)
+            )**np.float32(0.5))
+
+        def add_distances(df):
+            n_atoms = 1 + max([int(c.split('_')[1]) for c in df.columns if c.startswith('x_')])
+            
+            for i in range(1, n_atoms):
+                for vi in range(min(4, i)):
+                    add_distance_between(df, i, vi)
+
+        def add_n_atoms(base, structures):
+            dfs = structures['molecule_index'].value_counts().rename('n_atoms').to_frame()
+            return pd.merge(base, dfs, left_on='molecule_index', right_index=True)
+
+        def build_couple_dataframe(some_csv, structures_csv, coupling_type, n_atoms=10):
+            base, structures = build_type_dataframes(some_csv, structures_csv, coupling_type)
+            base = add_coordinates(base, structures, 0)
+            base = add_coordinates(base, structures, 1)
+            
+            base = base.drop(['atom_0', 'atom_1'], axis=1)
+            atoms = base.drop('id', axis=1).copy()
+            if 'scalar_coupling_constant' in some_csv:
+                atoms = atoms.drop(['scalar_coupling_constant'], axis=1)
+                
+            add_center(atoms)
+            atoms = atoms.drop(['x_0', 'y_0', 'z_0', 'x_1', 'y_1', 'z_1'], axis=1)
+
+            atoms = merge_all_atoms(atoms, structures)
+            
+            add_distance_to_center(atoms)
+            
+            atoms = atoms.drop(['x_c', 'y_c', 'z_c', 'atom_index'], axis=1)
+            atoms.sort_values(['molecule_index', 'atom_index_0', 'atom_index_1', 'd_c'], inplace=True)
+            atom_groups = atoms.groupby(['molecule_index', 'atom_index_0', 'atom_index_1'])
+            atoms['num'] = atom_groups.cumcount() + 2
+            atoms = atoms.drop(['d_c'], axis=1)
+            atoms = atoms[atoms['num'] < n_atoms]
+
+            atoms = atoms.set_index(['molecule_index', 'atom_index_0', 'atom_index_1', 'num']).unstack()
+            atoms.columns = [f'{col[0]}_{col[1]}' for col in atoms.columns]
+            atoms = atoms.reset_index()
+            
+            # downcast back to int8
+            for col in atoms.columns:
+                if col.startswith('atom_'):
+                    atoms[col] = atoms[col].fillna(0).astype('int8')
+                    
+            atoms['molecule_index'] = atoms['molecule_index'].astype('int32')
+            
+            full = add_atoms(base, atoms)
+            add_distances(full)
+            
+            full.sort_values('id', inplace=True)
+
+            print(coupling_type, "done!")
+            
+            return full
+
+        def take_n_atoms(df, n_atoms, four_start=4):
+            labels = []
+            for i in range(2, n_atoms):
+                label = f'atom_{i}'
+                labels.append(label)
+
+            for i in range(n_atoms):
+                num = min(i, 4) if i < four_start else 4
+                for j in range(num):
+                    labels.append(f'd_{i}_{j}')
+            if 'scalar_coupling_constant' in df:
+                labels.append('scalar_coupling_constant')
+            return df[labels]
+
+
+        types = train_csv["type"].unique()
+        train_df_list = []
+        test_df_list = []
+        for typ in types:
+            train_tmp = build_couple_dataframe(train_csv, structures_csv, typ, n_atoms=10)
+            test_tmp = build_couple_dataframe(test_csv, structures_csv, typ, n_atoms=10)
+            train_df_list.append(train_tmp)
+            test_df_list.append(test_tmp)
+
+        train_df = pd.concat(train_df_list, axis=0)
+        test_df = pd.concat(test_df_list, axis=0)
+        cols = [col for col in train_df.columns if "atom_" in col or "d_" in col]
+        cols.append("molecule_index")
+        train_df = train_df[cols]
+        test_df = test_df[cols]
+
+        train_csv = train_csv.merge(train_df, on=["molecule_index", "atom_index_0", "atom_index_1"], how="left")
+        test_csv = test_csv.merge(test_df, on=["molecule_index", "atom_index_0", "atom_index_1"], how="left")
+
+        new_cols = ['atom_2', 'atom_3', 'atom_4', 'atom_5', 'atom_6', 'atom_7', 'atom_8',
+            'atom_9', 'd_1_0', 'd_2_0', 'd_2_1', 'd_3_0',
+            'd_3_1', 'd_3_2', 'd_4_0', 'd_4_1', 'd_4_2', 'd_4_3', 'd_5_0', 'd_5_1',
+            'd_5_2', 'd_5_3', 'd_6_0', 'd_6_1', 'd_6_2', 'd_6_3', 'd_7_0', 'd_7_1',
+            'd_7_2', 'd_7_3', 'd_8_0', 'd_8_1', 'd_8_2', 'd_8_3', 'd_9_0', 'd_9_1',
+            'd_9_2', 'd_9_3']
+
+        for col in new_cols:
+            self.train[col] = train_csv[col]
+            self.test[col] = test_csv[col]     
+
 
 if __name__ == '__main__':
     args = get_arguments()
